528f3a3a95ebf40fafed16d7a41c910006310d80
GabrielDeSouzza <Gabrielemail4321@gmail.com>
2022-08-31 22:55:25 -0300

    Continue validations

--- Interface/CadastroTerceiros.cs
+++ Interface/CadastroTerceiros.cs
@@ -34,7 +34,7 @@ namespace Interface
         private void CadastroTerceiros_Resize(object sender, EventArgs e)
         {
             utils.alignCenterPanels(panelSerch, searchPanel, true, true);
-            utils.expansivePanels(10, panelCPF, panelTextCPFClient, panelEmail, panelTextEndereco, panelCNH,  panelFim);
+            utils.expansivePanels(10, panelCPF, panelTextCPFClient, panelEmail, panelTextEndereco, panelCNH, panelFim);
         }
 
         private void panelSerch_Paint(object sender, PaintEventArgs e)
